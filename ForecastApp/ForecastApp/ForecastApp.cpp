cc5f3234515958107ec464ac3cbe6dea8a74f748
Shubham More <u1429413@ad.utah.edu>
2024-02-06 10:59:11 -0700

    Project setup

---
+++ ForecastApp/ForecastApp/ForecastApp.cpp
@@ -0,0 +1,13 @@
+// ForecastApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <thread>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
